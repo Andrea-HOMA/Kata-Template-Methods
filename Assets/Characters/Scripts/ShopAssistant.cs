9f5b8347b4d059808bc1472e8734a6a03fc2d435
juan.blasco <juan.blasco@homagames.com>
2024-04-16 11:58:57 +0200

    Juan's ShopAssistant

--- Assets/Characters/Scripts/ShopAssistant.cs
+++ Assets/Characters/Scripts/ShopAssistant.cs
@@ -1,9 +1,44 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Characters.Scripts
 {
-    public class ShopAssistant : MonoBehaviour
+    public class ShopAssistant : Worker
     {
-        
+        protected override void MorningThoughts()
+        {
+            base.MorningThoughts();
+            Debug.Log("Today Im going to sell more than Kathy");
+        }
+
+        protected override void EatBreakfast()
+        {
+            Debug.Log("Not Having a big breakfast today, I think Im too fat");
+        }
+
+        protected override void GetDressed()
+        {
+            foreach (var mesh in _meshRenderers)
+            {
+                mesh.materials[0] = _outfitData.ShopAssistantOutfit;
+            }
+            Debug.Log("I look great!");
+        }
+
+        protected override IEnumerator GoToWork()
+        {
+            var storePosition = GameObject.Find("Store").transform.position;
+            var speed = 0.05f;
+            while (Vector3.Distance(storePosition, transform.position) > 0.5f)
+            {
+                transform.position += (storePosition - transform.position).normalized * speed;
+                yield return null;
+            }
+        }
+
+        protected override void Work()
+        {
+           Debug.Log("Lets sell some stufff!!!");
+        }
     }
 }
