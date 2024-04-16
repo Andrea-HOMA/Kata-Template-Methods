1398682e3c3c61b8186e06b00bb641c10f6b766d
shanickgauthier <shanick.gauthier.cuello@homagames.com>
2024-04-16 12:11:51 +0200

    Add teacher behaviour

--- Assets/Characters/Scripts/Teacher.cs
+++ Assets/Characters/Scripts/Teacher.cs
@@ -1,9 +1,41 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Characters.Scripts
 {
-    public class Teacher : MonoBehaviour
+    public class Teacher : Worker
     {
-        
+        private readonly string _school = "School";
+
+        protected override void MorningThoughts()
+        {
+            Debug.Log("I don't want to teach today...");
+        }
+
+        protected override void EatBreakfast()
+        {
+            Debug.Log("Eating children's tears for breakfast.");
+        }
+
+        protected override void GetDressed()
+        {
+        }
+
+        protected override IEnumerator GoToWork()
+        {
+            var school = GameObject.Find(_school).transform.position;
+            var speed = 0.05f;
+            while (Vector3.Distance(school, transform.position) > 0.5f)
+            {
+                transform.position += (school - transform.position).normalized * speed;
+                yield return null;
+            }
+        }
+
+        protected override void Work()
+        {
+            Debug.Log("I hate my job.");
+            Debug.Log("Teaching template methods to children.");
+        }
     }
 }
